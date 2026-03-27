5141b401c2a8ca7b68f2579f8d9435e9b7ebbcec
hamzaMissewi <hamza.missaoui47@gmail.com>
2026-03-27 23:16:50 +0100

    Initial commit: Super Mario EKS deployment with Terraform

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "mario12bucket"         # Ensure this is your actual S3 bucket
+    key            = "eks/terraform.tfstate" # Standardized directory naming
+    region         = "ap-south-1"            # Changed to match provider region
+    encrypt        = true                    # Ensures state file is encrypted
+    dynamodb_table = "terraform-lock"        # Enables state locking to prevent conflicts
+
+    use_lockfile = true # Enables state locking to prevent conflicts
+  }
+}
